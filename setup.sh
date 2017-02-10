fa0135b0ded03d5ebb7409620a8ba1f6d7e67119
Jon Gjengset <jon@thesquareplanet.com>
2017-02-10 13:59:19 -0500

    Student lab code release by jon

--- setup.sh
+++ setup.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-cd ~/secfs
-if [ $? -ne 0 ]; then
-	echo "SecFS directory not found, cannot continue."
-	exit 1
-fi
-
 rm -rf venv
 python3 -m venv --clear venv
 . venv/bin/activate
